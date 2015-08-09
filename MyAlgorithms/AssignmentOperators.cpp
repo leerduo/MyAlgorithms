51c3cc71467b69bb037a7557e4177ddccdf49daa
leerduo <eoeduo@163.com>
2015-08-10 07:49:56 +0800

    Assignment Operators

--- MyAlgorithms/AssignmentOperators.cpp
+++ MyAlgorithms/AssignmentOperators.cpp
@@ -12,3 +12,19 @@ public:
 private:
 	char *m_pData;
 };
+
+CMyString& CMyString::operator =(const CMyString &str)
+{
+	if (this == &str)
+	{
+		return *this;
+	}
+
+	delete[]m_pData;
+	m_pData = NULL;
+
+	m_pData = new char[strlen(str.m_pData) + 1];
+	strcpy(m_pData, str.m_pData);
+
+	return *this;
+}
